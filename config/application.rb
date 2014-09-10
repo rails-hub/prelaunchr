9271fd8838417e4143b844b79ae138f212752582
todd <todd@todd.(none)>
2014-09-10 17:39:04 +0500

    add changes

--- config/application.rb
+++ config/application.rb
@@ -56,13 +56,13 @@ module Prelaunchr
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
     config.ended = false
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
